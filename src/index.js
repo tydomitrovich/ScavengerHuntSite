3347f01139803d90b4666e69972a9ad00a99ba7c
tydomitrovich <tydomitrovich@bitbucket.org>
2018-03-16 12:00:09 -0700

    fixed input bar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import Notifications, {notify} from 'react-notify-toast';
 import './index.css';
 import block from './mystery_block.jpg';
 
+/*
+ * TODO: Add JSON Loading for QA
+ *
+ */
 
 class Prompt extends React.Component {
   render() {
@@ -24,7 +28,7 @@ class ScavengerHunt extends React.Component {
   constructor() {
     super();
     this.prompts = [
-      "There is a mystery box on floor 3 of the library. Feel inside and guess what is there. No Peeking!",
+      "There is a mystery box in the art center. Feel inside and guess what is there. No Peeking!",
       "You must SCAN one of the following areas for the answer you seek: The electric tower, the center of arts, the transit hub.",
       "Send a picture of people doing the YMCA to kahtnipp@gmail.com. You will receive the code you seek",
       "Decode the following: vqpuymefqd. HINT: Who are the Seahawks fans?",
@@ -45,9 +49,13 @@ class ScavengerHunt extends React.Component {
 
   checkAnswer(){
     var fmtGuess = this.currGuess.trim().toLowerCase();
-    if(fmtGuess == this.answers[this.state.currStep]){
+    if(fmtGuess === this.answers[this.state.currStep]){
       notify.show('Correct Answer!', 'success', 3000);
       this.setState({currStep: (this.state.currStep + 1)});
+      var inputBar = document.getElementsByClassName('submitBar');
+      if(inputBar !== undefined && inputBar.length > 0 ){
+      	inputBar[0].value = '';
+      }
     }else{
       notify.show('That is not the correct answer', 'error', 3000);
     }
@@ -57,11 +65,18 @@ class ScavengerHunt extends React.Component {
     this.currGuess = guess.target.value;
   }
 
+  handleSubmit(evt) {
+    if(evt.keyCode === 13) {
+    	this.checkAnswer();
+    }
+  }
+
   renderSubmitBar(){
     if(this.state.currStep <= this.maxSteps) {
       return (
-        <div className="submitBar">
-          <input className="submitBar" onChange={evt => this.updateGuess(evt)}/><span><button onClick={() => this.checkAnswer()}>Submit</button></span>
+        <div>
+          <input className="submitBar" onKeyDown={evt => this.handleSubmit(evt)}  onChange={evt => this.updateGuess(evt)}/>
+	  <span><button onClick={() => this.checkAnswer()}>Submit</button></span>
         </div>
       );
     }
@@ -75,7 +90,7 @@ class ScavengerHunt extends React.Component {
         <h3>If you complete all {this.maxSteps + 1} challenges, you will receive a code.</h3>
         <h3>This code will open a lockbox on top of the vending machines in Otto Miller Hall which contains a prize.</h3>
         <h3>All items involved with the hunt are marked with the question mark symbol.</h3>
-        <img src={block} height="200" width="200"/>
+        <img src={block} alt="Mystery Block" height="200" width="200"/>
         <br/>
         <div className="promptsContainer">
             <Prompt step={(this.state.currStep)} instructions={this.prompts[this.state.currStep]} />
